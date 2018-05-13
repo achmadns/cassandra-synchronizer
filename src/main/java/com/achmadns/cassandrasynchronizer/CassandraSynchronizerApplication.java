68f7b902ef6287b261688eedb6213ea249aa7cf6
Achmad Nasirudin Sandi <achmad.dev@gmail.com>
2018-05-14 06:39:06 +0700

    Initial commit

---
+++ src/main/java/com/achmadns/cassandrasynchronizer/CassandraSynchronizerApplication.java
@@ -0,0 +1,12 @@
+package com.achmadns.cassandrasynchronizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraSynchronizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CassandraSynchronizerApplication.class, args);
+    }
+}
